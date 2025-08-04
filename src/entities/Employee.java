359eba1137ce72b1e92fddad6e315c41a07433d2
Joao Lucas <jldecristo@gmail.com>
2025-08-03 21:57:30 -0300

    Construtores do Employee

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -6,4 +6,13 @@ public class Employee {
     private Integer hours;
     private Double valuePerHour;
 
+    public Employee() {
+    }
+
+    public Employee(String name, Integer hours, Double valuePerHour) {
+        this.name = name;
+        this.hours = hours;
+        this.valuePerHour = valuePerHour;
+    }
+
 }
