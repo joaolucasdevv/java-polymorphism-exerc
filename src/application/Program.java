71c86d63ef8a72cf96be6b70a8b5298c53e3d32f
Joao Lucas <jldecristo@gmail.com>
2025-08-03 22:25:07 -0300

    employee list

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,9 @@
 package application;
 
+import entities.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,6 +12,8 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        List<Employee> employees = new ArrayList<>();
 
         System.out.print("Enter the number of employees: ");
 
