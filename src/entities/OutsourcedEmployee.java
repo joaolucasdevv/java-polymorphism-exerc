d710ae2e09b498b203e8b1e7189dc89b2a5f5d9e
Joao Lucas <jldecristo@gmail.com>
2025-08-03 22:09:28 -0300

    construtores

--- src/entities/OutsourcedEmployee.java
+++ src/entities/OutsourcedEmployee.java
@@ -4,4 +4,12 @@ public class OutsourcedEmployee extends Employee {
 
     private Double additionalCharge;
 
+    public OutsourcedEmployee() {
+    }
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
 }
